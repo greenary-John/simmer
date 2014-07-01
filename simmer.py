bc5e612517b6f5cfbc88524c29667d17c73cd9b8
Patrick Osterhaus <pgosterhaus@gmail.com>
2014-07-01 15:14:54 -0400

    Integrated Extended_Closure to find reverse and forward closure.

--- simmer.py
+++ simmer.py
@@ -5,15 +5,20 @@ import optparse
 from icLib import Ontology
 from icLib import DAG
 from icLib import Config_Manager
+from icLib import Extended_Closure
 
 def main():
     options=optionParser()
     conman=Config_Manager.Config_Manager(options.configFile)
+    rclos=Extended_Closure.ReverseClosure()
+    fclos=Extended_Closure.ForwardClosure()
     print "Data Dir:\t",conman.getDataDir()
     print "Ont Dir:\t",conman.getOntDir()
     print "Ann Dir:\t",conman.getAnnDir(),"\n"
     ontologies=conman.getOntologies()
     annotations=conman.getAnnotations()
+    rclosure=rclos.multigo(ontologies[1])
+    fclosure=fclos.multigo(ontologies[1])
     #formatted printing of ontology namespaces and annotation subsets, respectively
     for x in range(0,len(ontologies[1])):
         print ontologies[0][x],":\t",ontologies[1][x].getNamespaces() 
@@ -21,6 +26,7 @@ def main():
     for x in range(0,len(annotations[1])):
         print annotations[0][x]," Preview:\t",annotations[1][x][0:2]
         print "\n"
+    
     #annotations[0] holds list of annotation types (e.g., "GO" or "MP")
     #annotations[1] holds list of annotations, split by tabs such that a list is
     #analogous to a row in Excel and each element in the list is from a unique column

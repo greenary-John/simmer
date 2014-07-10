4a43ab2ec6a865d2b44d9c9f5b66cfa5be42a077
Patrick Osterhaus <pgosterhaus@gmail.com>
2014-07-10 16:32:38 -0400

    Removed decrepit files.

--- icLib/Ontology_Annotation_Compiler.py
+++
@@ -1,9 +0,0 @@
-def AnnotationSetEvidenceFilter(AnnSet,evCodes):
-    #input of annotation set and desired evidence codes to REMOVE
-    for evCode in evCodes:
-        try:
-            AnnSet.annotations["ID"]={key:value for key,value in AnnSet.annotations["ID"].items() if value[1]["EvidenceCode"]!=evCode}
-            AnnSet.annotations["Obj"]={key:value for key,value in AnnSet.annotations["Obj"].items() if value[1]["EvidenceCode"]!=evCode}
-        except KeyError:
-            AnnSet.annotations["ID"]={key:value for key,value in AnnSet.annotations["ID"].items() if value[1]["Evidence"]!=evCode}
-            AnnSet.annotations["Obj"]={key:value for key,value in AnnSet.annotations["Obj"].items() if value[1]["Evidence"]!=evCode}

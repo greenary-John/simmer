d0da1648d0450214faad0f1fb175b74df368a45a
Joel Richardson <joel.richardson@jax.org>
2014-07-30 15:38:38 -0400

    Added a bare bones query form.

--- WSP.py
+++ WSP.py
@@ -17,13 +17,13 @@ from icLib import Logger
 from icLib import Labeler
 from icLib import SimmerEngine
 
-app= Flask(__name__)
+app= Flask(__name__, static_url_path="")
 
 #example input URL:
 #http://localhost:5000/?ecode=ND&annSet=geneGO&method=resnikBMA&qtype=object&qid=MGI:87961&length=25&nspace=biological_process
 
-@app.route('/')
-def hello_world():
+@app.route('/simmer')
+def simmer_engine():
     annSetChoice = request.values.get('annSet')
     evCodesChoice = request.values.getlist('ecode')
     searchType  = request.values.get('qtype')
@@ -40,14 +40,13 @@ def setConfigOptions(op):
 
 if __name__=='__main__':
     start=time.time()
-    print "Pre-Computation I..."
     logger=Logger.Logger()
+    print "Pre-Computation I..." 
     cm=ConfigManager.ConfigManager(setConfigOptions)
     simmercon=cm.readConfig()
     #readConfig() returns a SimmerConfigParser so simmercon is a SimmerConfigParser
     labeler=Labeler.Labeler(simmercon)
     ontman=OntologyManager.OntologyManager(simmercon)
     annman=AnnotationManager.AnnotationManager(simmercon,ontman)
-    simmercon,cm=None,None
     print time.time()-start
     app.run(debug=True,use_reloader=False)
